169ec62c3f6043ab5e8337e4fe8f74eee83e2bd3
Angel Marino <angel.marino@wennermedia.com>
2015-05-03 16:48:40 -0400

    Console logging and comments

--- tasks/CSV2JSON.js
+++ tasks/CSV2JSON.js
@@ -12,14 +12,14 @@ module.exports = function(grunt) {
 		
 	grunt.registerTask('csv2json', 'Turn CSVS into JSON', function(csvFileName) {
 
+		var done = this.async(); // Tell grunt to wait
+
 		var fs = require('fs');
 		
 		var csv = require('fast-csv');
 		
 		var JSONStream = require('JSONStream');
-		
-		var done = this.async(); // Grunt apparently isn't normally async friendly?
-			
+					
 		var inputFilePath = './'+grunt.config.get('csv2json.options.inputFilePath')+'/';
 		
 		var outputFilePath = './'+grunt.config.get('csv2json.options.outputFilePath')+'/';
@@ -30,28 +30,32 @@ module.exports = function(grunt) {
 		
 			if(!exists){
 		
-				console.log(inputFilePath+' file path does not exist! Specify a valid .csv file and directory.');	
+				grunt.log.error(inputFilePath+' file path does not exist! Specify a valid .csv file and directory.');	
 		
 			} else {
 				
-				var inStream = fs.createReadStream(inputFilePath+csvFileName+'.csv');
+				var inStream = fs.createReadStream(inputFilePath+csvFileName+'.csv'); // Read stream from CSV
+				
+				// Check if specified JSON directory is valid
 		
 				fs.exists(outputFilePath, function(exists){
 		
 					if(!exists){
 
-						console.log(outputFilePath+' file path does not exist! Specify a valid directory to save your JSON file.');
+						grunt.log.error(outputFilePath+' file path does not exist! Specify a valid directory to save your JSON file.'); // Grunt log missing JSON directory
 						
 					} else {
 						
-						var outStream = fs.createWriteStream('./'+outputFilePath+'/'+csvFileName+'.json');
+						var outStream = fs.createWriteStream('./'+outputFilePath+'/'+csvFileName+'.json'); // Create JSON write stream
 						
+						// Pipe CSV stream to JSONStream.stringify, then pipe to write stream
 						csv.fromStream(inStream, {objectMode: true, headers: true})
 						.pipe(JSONStream.stringify())
-						.pipe(outStream)
-						.on('end', function(){
-							console.log('JSON File Created');
-							done();
+						.pipe(outStream);
+						
+						outStream.on('finish', function(){
+							grunt.log.ok('JSON file successfully created');
+							done(); // Tell Grunt task is done						
 						});
 		
 					}
